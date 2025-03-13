15dea7d4423daac58842853cfb9039ef1e42299c
Rafli Ramadhani <rafliramadhani438@gmail.com>
2025-03-13 22:13:31 +0700

    under maintance for page :added

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,3 +1,6 @@
 <x-app-layout>
-    dashboard
+    <div class="flex h-full flex-col items-center justify-center gap-5">
+        <x-filament::icon icon="icon-under-construction" class="h-40 w-40 text-secondary dark:text-darkSecondary" />
+        <p class="text-center text-3xl uppercase">Working on progress! <br> This page will be available soon.</p>
+    </div>
 </x-app-layout>
